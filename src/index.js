51244002d9ebac20629e02b149eb732cc0713f7f
lahari <20b01a1233@svecw.edu.in>
2022-02-13 18:49:08 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CommentBox from "./CommentBox.js";
+
+const App =()=> {
+
+  return(
+      <>
+    <CommentBox />
+   
+    </>
+  )
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
